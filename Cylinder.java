0db2b41849efb275fd80b1bfbba22050bb7375d3
tulsib6 <tkb205@exeter.ac.uk>
2026-02-27 12:26:37 +0000

    updated

--- Cylinder.java
+++ Cylinder.java
@@ -3,31 +3,54 @@
  */
 
 // Task 1a. Edit the definition to make Cylinder a subclass of Circle
-//         
-
-public class Cylinder {
+//
+public class Cylinder extends Circle {
     
     // Task 1b. add additional private height attribute (double)
+    private double height;
 
 
     // Task 2. add code to define the following four constructors
     // no-arg constructor (set height to 1.0)
+    public Cylinder() {
+        super();
+        this.height = 1.0;
+    }
     // 1-arg constructor (set height to the given value)
+    public Cylinder (double height){
+        super();
+        this.height = height;
+    }
     // 2-arg constructor (set height and radius to the given values)
+    public Cylinder (double height, double radius){
+        super(radius);
+        this.height = height;
+    }
     // 3-arg constructor (set height, radius, and color to the given values)
-
-
+        public Cylinder(double height, double radius, String color) {
+            super(radius, color); // invoke superclass’ constructor
+            this.height = height;
+        }
     
     // Task 3. add getter and setter for height
     // (methods for radius and color are inherited)
+    public double getHeight() {
+        return this.height;
+    }
 
+    public void setHeight(double height) {
+        this.height = height;
+    }
 
     // Task 4. add code for method getVolume()
     // make use of superclass' getArea() method
-
+    public double getVolume() {
+        return super.getArea() * this.height;
+    }
  
     // Task 5. Override toString() method to describe itself
-    // (output format should be in line with format: Cylinder[Circle[...],height=X.XX]
-
-    
- }
+    // (output format should be in line with format: Cylinder[Circle[...],height=X.XX])
+    public String toString() {
+        return String.format ("Cylinder [%s, height = %.2f]", super.toString(), this.height);
+    }
+}
